ed2e6cff61b6f6ce7717f1525f40307d4d0ee392
alma2023 <ajay.patidar90989@gmail.com>
2023-10-31 15:55:56 +0530

    adding api

--- src/controler/userControler.js
+++ src/controler/userControler.js
@@ -1,4 +1,7 @@
 const UserModel = require("../models/userModel")
+const bcrypt = require("bcrypt")
+var jwt = require('jsonwebtoken');
+
 
 const addUser = async(req,res)=>{
     try{
@@ -69,4 +72,88 @@ const getUser = async(req,res)=>{
 }
 
 
-module.exports = {addUser,getUser}
+ const addData = async(req,res)=>{
+    
+    try{
+        bcrypt.hash(req.body.Password, 10,async function(err, hash) {
+            let detail = new UserModel({...req.body,Password:hash})
+        let success = await detail.save()
+        if(success){
+            res.status(200).json({
+                mssg:"it is added",
+                status:200,
+                success:success
+
+            })
+      }
+      
+    });
+  
+
+    }catch(err){
+        console.log(err)
+        res.status(500).json({
+            mssg:"it is not added",
+            status:500,
+            err:JSON.stringify(err)
+
+        })
+
+    }
+
+ }
+
+
+
+
+
+ 
+
+  let login = async(req,res)=>{
+    try{
+      
+        let {email,Password} = req.body
+        let user = await UserModel.find({email:email})
+        console.log(user)
+
+        if(user.length == 0){
+            res.status(400).json({
+                mssg:"user not found",
+                status:400,
+                user:user
+            })
+        }else{
+            bcrypt.compare(req.body.Password,user[0].Password,function(err, result) {
+
+                if(result == true){
+                    res.status(200).json({
+                        mssg:"login successful",
+                        status:200,
+                         user:user
+                    })
+
+                }else{
+                    res.status(400).json({
+                        mssg:"user not found",
+                        status:400,
+                        
+                    })
+
+                }
+                
+            });
+        }
+
+    }catch(err){
+        res.status(500).json({
+            mssg:"something wrong in function",
+            status:500,
+            err:JSON.stringify(err)
+        })
+
+    }
+  }
+
+
+
+module.exports = {addUser,getUser,login,addData}
